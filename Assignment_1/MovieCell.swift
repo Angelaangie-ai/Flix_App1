c544173f699f958bdc01208cc45f2c74a11d97c8
Angela Busheska <busheska@lafayette.edu>
2022-02-26 15:33:34 -0500

    An app to showcase movies

---
+++ Assignment_1/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  Assignment_1
+//
+//  Created by Apple on 25/02/2022.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Assignment_1/MovieeCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieeCell.swift
+//  Assignment_1
+//
+//  Created by Apple on 26/02/2022.
+//
+
+import UIKit
+
+class MovieeCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UILabel!
+
+
+
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
